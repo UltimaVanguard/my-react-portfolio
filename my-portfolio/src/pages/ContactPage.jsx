4ad095a67dac51e4095a3efb417ecec3323d1c01
UltimaVanguard <ultimavanguard@gmail.com>
2024-05-30 09:35:23 -0500

    finished contact page

--- my-portfolio/src/pages/ContactPage.jsx
+++ my-portfolio/src/pages/ContactPage.jsx
@@ -1,10 +1,49 @@
-export default function ContactPage() {
+import { useState } from 'react';
+
+function ContactPage() {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [content, setContent] =useState('');
+
     return (
         <div className="m-3 p-3">
             <h2 className="text-center">Contact Me</h2>
-            <p>
-
-            </p>
+            {/* <form className="ui form" onSubmit={sendTerm}> */}
+            <form className="d-flex flex-row flex-wrap justify-content-center align-items-center">
+                <div className="p-3 col-12 col-lg-4 d-flex justify-content-center">
+                    <label className='p-1'>Name:</label>
+                    <input
+                        type="text"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                        placeholder="Name"
+                    />
+                </div>
+                <div className="d-flex justify-content-center p-3 col-12 col-lg-4">
+                    <label className='p-1'>Email:</label>
+                    <input
+                        type="text"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        placeholder="Email"
+                    />
+                </div>
+                <div className="p-3 col-12 col-lg-10 d-flex align-items-top justify-content-center">
+                    <label className='p-1'>Message:</label>
+                    <textarea
+                        value={content}
+                        onChange={(e) => setContent(e.target.value)}
+                        placeholder="Message"
+                    ></textarea>
+                </div>
+                <div className="col-12 col-lg-3 d-flex justify-content-center">
+                    <button className="btn btn-primary btn-block py-3" type="submit">
+                        Send Message
+                    </button>
+                </div>
+            </form>
         </div>
     );
-  }
+}
+
+export default ContactPage;
