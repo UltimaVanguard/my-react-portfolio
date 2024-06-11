65b67238344b611be65889fe50cca12926ea15b9
UltimaVanguard <ultimavanguard@gmail.com>
2024-06-11 11:47:43 -0500

    added an a in the about me section

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -3,7 +3,7 @@ export default function About() {
         <div className="m-3 p-3 d-flex flex-column align-items-center">
             <h2>About Me</h2>
             <p className="about-me col-6 text-center">
-            I am results-oriented professional with five years of experience in COBOL development, now returning to the tech industry after a six-year period focused on personal and professional growth. 
+            I am a results-oriented professional with five years of experience in COBOL development, now returning to the tech industry after a six-year period focused on personal and professional growth. 
             During this time, I have maintained and further developed my problem-solving, attention to detail, and collaboration skills. I am eager to re-enter the field of technology and transition into Full Stack Web Development, 
             leveraging my past experience and a commitment to staying updated with current industry trends and technologies.
             </p>
